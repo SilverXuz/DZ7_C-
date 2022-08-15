acfa1c91aa7ecd6d43e484e0c6f84ffdcfc3e688
SilverXuz <silver-xuz@mail.ru>
2022-08-16 00:23:15 +0300

    Считает среднее арифметическое элементов в каждом столбце

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -18,9 +18,9 @@ int[] arrayM = new int[m];
 int sum = 0;
 void Array(int[,] matrix)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
             matrix[i, j] = new Random().Next(0,10);
             Console.Write(matrix[i, j] + " \t");
@@ -28,9 +28,9 @@ void Array(int[,] matrix)
         Console.WriteLine();
     }
 
-    for (int j = 0; j < m; j++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
          sum += matrix[i, j];
          arrayM[j] = sum;
